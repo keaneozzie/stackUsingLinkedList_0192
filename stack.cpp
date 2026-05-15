034ab8a8fe71b43aeed3f7fdf9da252ec395dabd
keaneozzie <ozziedharma897@gmail.com>
2026-05-15 16:34:41 +0700

    add menu-based user interface

--- stack.cpp
+++ stack.cpp
@@ -84,4 +84,9 @@ int main()
     int choice = 0;
     int value;
 
-    
+    while (choice != 5)
+    {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        
